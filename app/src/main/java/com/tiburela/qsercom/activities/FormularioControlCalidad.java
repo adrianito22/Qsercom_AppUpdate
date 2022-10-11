58d028c9ab78ab2970e494b77d3ccf762840baa8
adriano <adriapps10@gmail.com>
2022-10-11 18:50:01 -0500

    committ 16: 56

--- app/src/main/java/com/tiburela/qsercom/activities/FormularioControlCalidad.java
+++ app/src/main/java/com/tiburela/qsercom/activities/FormularioControlCalidad.java
@@ -3,22 +3,30 @@ package com.tiburela.qsercom.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
+import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.tiburela.qsercom.R;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class FormularioControlCalidad extends AppCompatActivity implements View.OnClickListener {
     // initialize variables
 
     TextView textView;
     boolean[] selectedLanguage;
+
+    TextView textView48;
+
 
     //Imageviews defects
     ImageView imgSelecDefc1;
@@ -33,7 +41,52 @@ public class FormularioControlCalidad extends AppCompatActivity implements View.
     ImageView imgSelecDefc10;
 
 
-    //imgvEMPAQUES
+
+
+     TextInputEditText	edizz1	;
+     TextInputEditText	edizz2	;
+     TextInputEditText	edizz3	;
+     TextInputEditText	edizz4	;
+     TextInputEditText	edizz5	;
+     TextInputEditText	edizz6	;
+     TextInputEditText	edizz7	;
+     TextInputEditText	edizz8	;
+     TextInputEditText	edizz9	;
+     TextInputEditText	edizz10	;
+     TextInputEditText	edizz11	;
+     TextInputEditText	edizz12	;
+     TextInputEditText	edizz13	;
+     TextInputEditText	edizz14	;
+     TextInputEditText	edizz15	;
+     TextInputEditText	edizz16	;
+     TextInputEditText	edizz17	;
+     TextInputEditText	edizz18	;
+     TextInputEditText	edizz19	;
+     TextInputEditText	edizz20	;
+     TextInputEditText	edizz21	;
+     TextInputEditText	edizz22	;
+     TextInputEditText	edizz23	;
+     TextInputEditText	edizz24	;
+     TextInputEditText	edizz25	;
+     TextInputEditText	edizz26	;
+     TextInputEditText	edizz27	;
+     TextInputEditText	edizz28	;
+     TextInputEditText	edizz29	;
+     TextInputEditText	edizz30	;
+     TextInputEditText	edizz31	;
+     TextInputEditText	edizz32	;
+     TextInputEditText	edizz33	;
+     TextInputEditText	edizz34	;
+     TextInputEditText	edizz35	;
+     TextInputEditText	edizz36	;
+     TextInputEditText	edizz37	;
+     TextInputEditText	edizz38	;
+     TextInputEditText	edizz39	;
+     TextInputEditText	edizz40	;
+
+
+
+     //imgvEMPAQUES
 
     ImageView imvEmpaque1;
     ImageView imvEmpaque2;
@@ -56,6 +109,22 @@ public class FormularioControlCalidad extends AppCompatActivity implements View.
     Spinner spinnerDef8;
     Spinner spinnerDef9;
     Spinner spinnerDef10;
+
+
+    TextView txtTotal1;
+    TextView txtTotal2;
+    TextView txtTotal3;
+    TextView txtTotal4;
+    TextView txtTotal5;
+    TextView txtTotal6;
+    TextView txtTotal7;
+    TextView txtTotal8;
+    TextView txtTotal9;
+    TextView txtTotal10;
+
+    TextView txtTotal;
+
+
 
 
     ArrayList<Integer> langList = new ArrayList<>();
@@ -140,11 +209,51 @@ public class FormularioControlCalidad extends AppCompatActivity implements View.
 
     private void findviewsIds() {
 
-         imgSelecDefc1=findViewById(R.id.imgSelecDefc1);
+        edizz1=findViewById(R.id.edizz1);
+        edizz2=findViewById(R.id.edizz2);
+        edizz3=findViewById(R.id.edizz3);
+        edizz4=findViewById(R.id.edizz4);
+        edizz5=findViewById(R.id.edizz5);
+        edizz6=findViewById(R.id.edizz6);
+        edizz7=findViewById(R.id.edizz7);
+        edizz8=findViewById(R.id.edizz8);
+        edizz9=findViewById(R.id.edizz9);
+        edizz10=findViewById(R.id.edizz10);
+        edizz11=findViewById(R.id.edizz11);
+        edizz12=findViewById(R.id.edizz12);
+        edizz13=findViewById(R.id.edizz13);
+        edizz14=findViewById(R.id.edizz14);
+        edizz15=findViewById(R.id.edizz15);
+        edizz16=findViewById(R.id.edizz16);
+        edizz17=findViewById(R.id.edizz17);
+        edizz18=findViewById(R.id.edizz18);
+        edizz19=findViewById(R.id.edizz19);
+        edizz20=findViewById(R.id.edizz20);
+        edizz21=findViewById(R.id.edizz21);
+        edizz22=findViewById(R.id.edizz22);
+        edizz23=findViewById(R.id.edizz23);
+        edizz24=findViewById(R.id.edizz24);
+        edizz25=findViewById(R.id.edizz25);
+        edizz26=findViewById(R.id.edizz26);
+        edizz27=findViewById(R.id.edizz27);
+        edizz28=findViewById(R.id.edizz28);
+        edizz29=findViewById(R.id.edizz29);
+        edizz30=findViewById(R.id.edizz30);
+        edizz31=findViewById(R.id.edizz31);
+        edizz32=findViewById(R.id.edizz32);
+        edizz33=findViewById(R.id.edizz33);
+        edizz34=findViewById(R.id.edizz34);
+        edizz35=findViewById(R.id.edizz35);
+        edizz36=findViewById(R.id.edizz36);
+        edizz37=findViewById(R.id.edizz37);
+        edizz38=findViewById(R.id.edizz38);
+        edizz39=findViewById(R.id.edizz39);
+        edizz40=findViewById(R.id.edizz40);
+
+
+        imgSelecDefc1=findViewById(R.id.imgSelecDefc1);
          imgSelecDefc2=findViewById(R.id.imgSelecDefc2);
          imgSelecDefc3=findViewById(R.id.imgSelecDefc3);
-
-
          imgSelecDefc4=findViewById(R.id.imgSelecDefc4);
          imgSelecDefc5=findViewById(R.id.imgSelecDefc5);
          imgSelecDefc6=findViewById(R.id.imgSelecDefc6);
@@ -177,7 +286,21 @@ public class FormularioControlCalidad extends AppCompatActivity implements View.
         spinnerDef1=findViewById(R.id.spinnerDef10);
 
 
+        txtTotal1=findViewById(R.id.txtTotal1);
+        txtTotal2=findViewById(R.id.txtTotal2);
+        txtTotal3=findViewById(R.id.txtTotal3);
+        txtTotal4=findViewById(R.id.txtTotal4);
+        txtTotal5=findViewById(R.id.txtTotal5);
+        txtTotal6=findViewById(R.id.txtTotal6);
+        txtTotal7=findViewById(R.id.txtTotal7);
+        txtTotal8=findViewById(R.id.txtTotal8);
+        txtTotal9=findViewById(R.id.txtTotal9);
+        txtTotal10=findViewById(R.id.txtTotal10);
 
+
+        textView48=findViewById(R.id.textView48);
+
+        txtTotal=findViewById(R.id.txttotal);
 
     }
 
@@ -207,8 +330,88 @@ public class FormularioControlCalidad extends AppCompatActivity implements View.
         imvEmpaque9.setOnClickListener(this);
         imvEmpaque10.setOnClickListener(this);
 
+        edizz1.setOnClickListener(this);
+        edizz5.setOnClickListener(this);
+        edizz9.setOnClickListener(this);
+        edizz13.setOnClickListener(this);
+        edizz17.setOnClickListener(this);
+        edizz21.setOnClickListener(this);
+        edizz25.setOnClickListener(this);
+        edizz29.setOnClickListener(this);
+        edizz33.setOnClickListener(this);
+        edizz37.setOnClickListener(this);
+
+        textView48.setOnClickListener(this);
+
+
 
     }
+
+
+
+    void muestraResultado()  {
+
+      ArrayList <Integer> result10 = new ArrayList<Integer>();
+
+        int resultadoSuMA;
+
+        for(int indice2=0; indice2<listOfLISTState.size(); indice2++){  //lista de listas
+            resultadoSuMA=0;
+
+            for(int indice=0; indice<listOfLISTState.get(indice2).size(); indice++){  //recorremos la lista actual
+
+
+                if(listOfLISTState.get(indice2).get(indice)){
+                    resultadoSuMA=resultadoSuMA+1;
+
+                }
+
+
+
+
+            }
+
+
+            result10.add(resultadoSuMA);
+            //aqui ya debemos tener el resultado
+
+
+        }
+
+
+
+
+int resultadotodo=0;
+        for(int indice=0; indice<result10.size(); indice++){  //recorremos la lista actual
+
+
+            resultadotodo=resultadotodo+result10.get(indice);
+
+
+Log.i("sumarr","el valor es "+result10.get(indice));
+
+
+        }
+
+        txtTotal.setText(String.valueOf(resultadotodo));
+
+
+        txtTotal1.setText(String.valueOf(result10.get(0)));
+        txtTotal2.setText(String.valueOf(result10.get(1)));
+        txtTotal3.setText(String.valueOf(result10.get(2)));
+        txtTotal4.setText(String.valueOf(result10.get(3)));
+        txtTotal5.setText(String.valueOf(result10.get(4)));
+        txtTotal6.setText(String.valueOf(result10.get(5)));
+        txtTotal7.setText(String.valueOf(result10.get(6)));
+        txtTotal8.setText(String.valueOf(result10.get(7)));
+        txtTotal9.setText(String.valueOf(result10.get(8)));
+        txtTotal10.setText(String.valueOf(result10.get(9)));
+
+
+
+    }
+
+
 
 
 
@@ -362,8 +565,76 @@ public class FormularioControlCalidad extends AppCompatActivity implements View.
 
 
 
+            case R.id.edizz1:
+
+                showingTimePicker(view);
+                break;
 
 
+            case R.id.edizz5:
+
+                showingTimePicker(view);
+                break;
+
+
+            case R.id.edizz9:
+
+                showingTimePicker(view);
+                break;
+
+
+
+            case R.id.edizz13:
+
+                showingTimePicker(view);
+                break;
+
+
+
+            case R.id.edizz17:
+
+                showingTimePicker(view);
+                break;
+
+
+            case R.id.edizz21:
+
+                showingTimePicker(view);
+                break;
+
+
+
+            case R.id.edizz25:
+
+                showingTimePicker(view);
+                break;
+
+
+
+            case R.id.edizz29:
+
+                showingTimePicker(view);
+                break;
+
+
+            case R.id.edizz33:
+
+                showingTimePicker(view);
+                break;
+
+
+            case R.id.edizz37:
+
+                showingTimePicker(view);
+                break;
+
+
+
+            case R.id.textView48:
+
+                muestraResultado();
+
+                break;
         }
 
     }
@@ -550,6 +821,109 @@ public class FormularioControlCalidad extends AppCompatActivity implements View.
 
 
     }
+
+
+
+
+
+
+    void showingTimePicker( View vista){
+
+        final Calendar cldr = Calendar.getInstance();
+        int hour = cldr.get(Calendar.HOUR_OF_DAY);
+        int minutes = cldr.get(Calendar.MINUTE);
+        // time picker dialog
+        TimePickerDialog picker = new TimePickerDialog(FormularioControlCalidad.this,
+                new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker tp, int sHour, int sMinute) {
+                        if(vista.getId()==R.id.edizz1) {
+                            edizz1.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+                        else if (vista.getId()== R.id.edizz5) {
+                            edizz5.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+
+                        else if (vista.getId()== R.id.edizz9) {
+                            edizz9.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+
+                        else if (vista.getId()== R.id.edizz13) {
+                            edizz13.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+                        else if (vista.getId()== R.id.edizz17) {
+                            edizz17.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+
+                        else if (vista.getId()== R.id.edizz21) {
+                            edizz21.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+                        else if (vista.getId()== R.id.edizz25) {
+                            edizz25.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+                        else if (vista.getId()== R.id.edizz29) {
+                            edizz29.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+
+
+                        else if (vista.getId()== R.id.edizz33) {
+                            edizz33.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+
+                        else if (vista.getId()== R.id.edizz37) {
+                            edizz37.setText(sHour + ":" + sMinute);
+
+
+                        }
+
+
+
+
+
+                    }
+                }, hour, minutes, true);
+
+        picker.setButton(DialogInterface.BUTTON_POSITIVE, "OK", picker);
+        picker.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancelar", picker);
+
+        picker.show();
+    }
+
 
 
 }
