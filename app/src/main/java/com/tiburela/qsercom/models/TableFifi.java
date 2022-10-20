387ac094e4a3ae7e261ed8a733bb69a4398a0f59
adrianito22 <79810977+adrianito22@users.noreply.github.com>
2022-10-20 09:50:03 -0500

    coomit n house

---
+++ app/src/main/java/com/tiburela/qsercom/models/TableFifi.java
@@ -0,0 +1,82 @@
+package com.tiburela.qsercom.models;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
+import java.util.ArrayList;
+
+public class TableFifi {
+
+
+    Paint paintLines;
+    int posicionYEstarTable;
+    int posicionYendTable;
+    int marginLefTable;
+    int marginRigthTable;
+    int numFiles; //el size de la lista
+    int numColumnas;
+    String backGroundColorTable;
+   int totalOcupa;  //puedes ser la mita o todoxx o cutoms cantidad
+
+
+    //un metodo para crear una tabla......
+    ///queremos pasarle
+
+public TableFifi(int poscionStarTableY,Paint paintLines, ArrayList<String> namesFieldsAndTotalColumna,int totalOcupaOmodo){  ///para header y table..
+
+
+
+
+
+
+    //names fields.. si le pasa 3 fields....
+
+    //podemos agregarle un header a la tabla....
+}
+
+
+
+//tambien deberia haber uno para solo crear tabla y hedader....
+
+
+    public void addTAbleWhitHeader( TableFifi tableFifi){  ///le pasmoa un objeto table whit header......
+
+
+
+
+    }
+
+    //table whitHeader only color...
+
+    //Table table = new table(); //patrametros...
+
+    ////add table
+
+
+    //table whit color column color...
+
+
+
+
+
+
+    //table whit text..........
+
+
+
+    //table whitouColor.
+
+
+
+    //metodo agrega background color in celd.........
+    ///entonces cuando quremos un header color usamos este metodo....
+
+
+    //si es multicolor......
+
+
+
+  //emetodo delete table...background  ....
+
+
+}
